72f2a43132ea15a5750e0b1b5b76fdbe159c5c74
Pavel-shl <pasha.shlyaga@mail.ru>
2021-12-01 00:26:05 +0300

    Add 3 lab

---
+++ AssemblyBrowser/AssemblyBrowser/AssemblyViever.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using AssemblyBrowser.DescriptionsGenerators;
+
+
+namespace AssemblyBrowser
+{
+    public class AssemblyViever
+    {
+        //список нэймспейсов
+        private List<NamespaceDescription> namespaces1 = new List<NamespaceDescription>();
+
+        
+        public List<NamespaceDescription> namespaces { get { return namespaces1; } }
+
+        public string AssemblyName { get; private set; }
+
+        //просмотреть сборку, передается путь dll
+        public void VievAssembly( string path )
+        {
+            this.CloseAssembly();
+            //загружается файл
+            Assembly assembly =Assembly.LoadFrom(path); 
+            // получаются все типы из сборки
+            Type[] types = assembly.GetTypes();
+            //анализаторы создаются экземпляры, для описания классов служат
+            NamespaceAnalizer analizer = new NamespaceAnalizer();
+            ClassAnalizer classAnalizer = new ClassAnalizer();
+
+            NamespaceDescription current;
+            ClassDescription currentClass;
+
+            //цикл по всем типам, просматриваются все типы, добавляется их описание в структуру данных
+            foreach (Type type in types)
+            {
+                //если не класс то пропускаем
+                if (!type.IsClass)
+                    continue;
+                //смотрим нэймспейс есть есть то добавляем в существующий если нет то создаем новый
+                current = namespaces.Find((NamespaceDescription description) => (description.Name == type.Namespace));
+                if (  current == null ) 
+                {
+                    current = analizer.GenerateNamespaceDescription(type.Namespace);
+                    namespaces.Add(current);
+                } 
+                     
+                currentClass = classAnalizer.GenerateClassDescription(type);
+                current.Classes.Add(currentClass);
+            }
+        }
+
+        //для очистки программы
+        public void CloseAssembly()
+        {
+            namespaces1 = new List<NamespaceDescription>();
+            AssemblyName = "";
+
+        }
+
+    }
+}
